ad94ea06797529905fa0716126955c41105069ae
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2024-10-08 10:31:05 +0700

    LEARN : Update Address

--- restfullapi/src/controller/address-controller.js
+++ restfullapi/src/controller/address-controller.js
@@ -34,7 +34,27 @@ const get = async (req, res, next) => {
     }
 }
 
+// Update
+const update = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const contactId = req.params.contactId // Get From URL Params
+        const addressId = req.params.addressId // Get From URL Params
+        const request = req.body;
+        request.id = addressId
+        
+        const result = await addressService.update(user, contactId, request);
+        
+        res.status(200).json({
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
 export default {
     create,
-    get
+    get,
+    update
 }
