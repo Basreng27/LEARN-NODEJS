24733f9dc9e8d9eb24d1e47ec2d787dca125d632
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2024-10-09 13:34:44 +0700

    DONE : LEARN NODE JS API

--- restfullapi/src/main.js
+++ restfullapi/src/main.js
@@ -2,6 +2,6 @@ import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
 // 300 Is PORT
-web.listen(300, () => {
+web.listen(3000, () => {
     logger.info("APP Start");
 }) 
